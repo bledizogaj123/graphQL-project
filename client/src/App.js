12d0b51cb33b6440ec56c63c2208dc0ee41f2b37
Agent 3 <agent.3@VENTIUS.LOCAL>
2023-02-06 14:03:11 +0100

    Getting started with front-end

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import Header from './components/Header';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import Clients from './components/Clients';
+
+const cache  = new InMemoryCache({
+  typePolicies:{
+    Query:{
+      fields:{
+        clients:{
+          merge(existing, incoming) {
+            return incoming;
+          }
+        }
+      }
+    }
+  }
+});
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache(),
+})
+
+function App() {
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <Clients />
+        </div>
+      </ApolloProvider>
+    </>
+  );
+}
+
+export default App;
