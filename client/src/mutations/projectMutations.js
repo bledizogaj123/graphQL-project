8b15f789c2625512d5e566ff4a1c94441e2e1560
bledizogaj123 <bz47140@ubt-uni.net>
2023-02-08 11:14:21 +0100

    Final commit

--- client/src/mutations/projectMutations.js
+++ client/src/mutations/projectMutations.js
@@ -1,9 +1,17 @@
 import { gql } from '@apollo/client';
 
 const ADD_PROJECT = gql`
-        mutation AddProject ($name: String!, $description: String!, $status:ProjectStatus! , $clientId: ID!)
+        mutation AddProject(
+            $name: String!, 
+            $description: String!, 
+            $status:ProjectStatus! , 
+            $clientId: ID!)
         {
-            addProject(name: $name, description: $description, status: $status, clientId: $clientId)
+            addProject(
+                name: $name, 
+                description: $description, 
+                status: $status, 
+                clientId: $clientId)
             {
                 id
                 name
@@ -20,4 +28,41 @@ const ADD_PROJECT = gql`
             }
         }
 `;
-export {ADD_PROJECT};
+
+const UPDATE_PROJECT = gql`
+  mutation UpdateProject(
+    $id: ID!
+    $name: String!
+    $description: String!
+    $status: ProjectStatusUpdate!
+  ) {
+    updateProject(
+      id: $id
+      name: $name
+      description: $description
+      status: $status
+    ) {
+      id
+      name
+      description
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
+  }
+`;
+
+const DELETE_PROJECT = gql`
+        mutation DeleteProejct($id: ID!)
+        {
+            deleteProject(id: $id)
+            {
+                id
+            }
+        }
+`
+export { ADD_PROJECT,  DELETE_PROJECT, UPDATE_PROJECT };
